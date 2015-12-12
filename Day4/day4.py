a118be1ad7b000f76bbc143771181a88a02afcfe
David Lebrun <dave.lebr1@gmail.com>
2015-12-11 21:30:21 -0500

    Changing structure

--- Day4/day4.py
+++ Day4/day4.py
@@ -5,11 +5,13 @@ import hashlib
 secretkey = "ckczppom"
 finded = False
 answer = ""
+i = 0
 
-for i in range(0,10000000):
+while True:
     if hashlib.md5(secretkey+str(i)).hexdigest().startswith("00000") and finded is False:
         print ("Answer for 5 zero is : " + str(i))
         finded = True
     if hashlib.md5(secretkey+str(i)).hexdigest().startswith("000000"):
         print ("Answer with 6 zero is : " + str(i))
         break
+    i+= 1
